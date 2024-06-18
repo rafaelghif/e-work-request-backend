4c25a65a15d7621a52c22490292cfd97a7560537
Muhammad Rafael Ghifari <business.rafaelghifari@gmail.com>
2024-06-18 09:28:08 +0700

    refactor: database connection

--- configs/database.js
+++ configs/database.js
@@ -1,27 +1,28 @@
-import * as dotenv from "dotenv";
-dotenv.config();
 import { Sequelize } from "sequelize";
 
+const getDatabaseOptions = (env) => {
+	const options = {
+		Development: {
+			host: process.env.DEV_HOST,
+			port: process.env.DEV_PORT,
+			database: process.env.DEV_DATABASE,
+			username: process.env.DEV_USERNAME,
+			password: process.env.DEV_PASSWORD,
+		},
+		Production: {
+			host: process.env.PROD_HOST,
+			port: process.env.PROD_PORT,
+			database: process.env.PROD_DATABASE,
+			username: process.env.PROD_USERNAME,
+			password: process.env.PROD_PASSWORD,
+		},
+	};
+
+	return options[env] ?? options["Development"];
+};
+
 const environment = process.env.NODE_ENV ?? "Development";
-
-const developmentOptions = {
-	host: process.env.DEV_HOST,
-	port: process.env.DEV_PORT,
-	database: process.env.DEV_DATABASE,
-	username: process.env.DEV_USERNAME,
-	password: process.env.DEV_PASSWORD,
-};
-
-const productionOptions = {
-	host: process.env.PROD_HOST,
-	port: process.env.PROD_PORT,
-	database: process.env.PROD_DATABASE,
-	username: process.env.PROD_USERNAME,
-	password: process.env.PROD_PASSWORD,
-};
-
-const connectionOptions =
-  environment === "Production" ? productionOptions : developmentOptions;
+const connectionOptions = getDatabaseOptions(environment);
 
 const connectionDatabase = new Sequelize(
 	connectionOptions.database,
@@ -39,13 +40,18 @@ const connectionDatabase = new Sequelize(
 			idle: 20000,
 		},
 		logging: false,
-	}
+	},
 );
 
-try {
-	console.log("Connection has been established successfully.");
-} catch (error) {
-	console.error("Unable to connect to the database:", error);
-}
+const testConnection = async () => {
+	try {
+		await connectionDatabase.authenticate();
+		console.log("Connection has been established successfully.");
+	} catch (error) {
+		console.error("Unable to connect to the database:", error);
+	}
+};
+
+testConnection();
 
 export default connectionDatabase;
